0e0b662c3cf50efe614eaa19f2ab897f14983d4d
GoGMI-Ghana <info@gogmi.org.gh>
2026-07-06 16:41:10 +0100

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -157,7 +157,7 @@ const Footer = () => {
                   <Youtube className="w-5 h-5" />
                 </a>
                 <a 
-                  href="https://wa.me/233504953400" 
+                  href="https://www.youtube.com/@gulfofguineamaritimeinstitute" 
                   target="_blank"
                   rel="noopener noreferrer"
                   className="w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center hover:bg-green-500 hover:text-white transition-all hover:scale-110"
